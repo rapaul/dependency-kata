7639a8ec120d46a7d4b586508cccf2b07d293592
Richard Paul <richard.a.paul@gmail.com>
2013-02-18 20:58:51 +1300

    Passing test for single direct dependency

--- src/Dependencies.java
+++ src/Dependencies.java
@@ -1,21 +1,23 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 
 public class Dependencies {
 
+	private Dependency dependency;
+
 	public List<Dependency> getDependencyFor(String string) {
 		return new LinkedList<Dependency>();
 	}
 
 	public void addDirect(Dependency item, Dependency dependency) {
-		// TODO Auto-generated method stub
+		this.dependency = dependency;
 		
 	}
 
 	public List<Dependency> getDependencyFor(Dependency item) {
-		// TODO Auto-generated method stub
-		return null;
+		return Arrays.asList(dependency);
 	}
 
 }
