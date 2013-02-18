2fe0c87cebb59b98a969075ee4af215f34640f53
Richard Paul <richard.a.paul@gmail.com>
2013-02-18 21:37:48 +1300

    Refactored to handle symmetric correctly, transitive now failing

--- src/Dependencies.java
+++ src/Dependencies.java
@@ -1,17 +1,31 @@
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 
 public class Dependencies {
 
-	private List<Dependency> dependencies = new LinkedList<Dependency>();
+	private Map<Dependency, List<Dependency>> dependencies = new HashMap<Dependency, List<Dependency>>();
 
 	public void addDirect(Dependency item, Dependency dependency) {
-		this.dependencies.add(dependency);
+		List<Dependency> directDependencies = lazyInitialiseFor(item);
+		directDependencies.add(dependency);
+	}
+
+	private List<Dependency> lazyInitialiseFor(Dependency item) {
+		List<Dependency> directDependencies = dependencies.get(item);
+		if (directDependencies == null) {
+			directDependencies = new LinkedList<Dependency>();
+			dependencies.put(item, directDependencies);
+		}
+		return directDependencies;
 	}
 
 	public List<Dependency> getDependencyFor(Dependency item) {
-		return dependencies;
+		List<Dependency> deps = dependencies.get(item);
+		return deps == null ? Collections.<Dependency>emptyList() : deps;
 	}
 
 }
